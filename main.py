cf0ecefdef1460f418c6d69853eda77d6cc6f1e8
sameel prayurathanes <sameel.p@ku.th>
2025-12-26 14:57:11 +0700

    covid

--- main.py
+++ main.py
@@ -21,3 +21,22 @@ def index():
                            name=name, 
                            time=time,
                            news_items=news_items.values())
+@app.route('/news/<id>/')
+def show_news_item(id):
+
+    news_items = {
+    1: {'id': 1,
+        'title': 'COVID-19 update',
+        'body': 'This is a news on COVID-19'},
+    2: {'id': 2,
+        'title': 'Facemasks found',
+        'body': 'Recent news on facemask production'},
+    3: {'id': 3,
+        'title':'Python 4',
+        'body':'Python 4 will be out soon.... this is FAKE'},
+}
+    news_item = news_items[int(id)]
+    return render_template('news_item.html',
+                           id=news_item['id'],
+                           title=news_item['title'],
+                           body=news_item['body'])
