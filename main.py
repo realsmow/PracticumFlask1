b9267ee60c7818d429ef121e4383d51445ff9ff3
sameel prayurathanes <sameel.p@ku.th>
2025-12-26 14:40:41 +0700

    add list

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    name = "bubble"
+    time = 24
+    news_items = {
+    1: {'id': 1,
+        'title': 'COVID-19 update',
+        'body': 'This is a news on COVID-19'},
+    2: {'id': 2,
+        'title': 'Facemasks found',
+        'body': 'Recent news on facemask production'},
+    3: {'id': 3,
+        'title':'Python 4',
+        'body':'Python 4 will be out soon.... this is FAKE'},
+}
+    return render_template('index.html', 
+                           name=name, 
+                           time=time,
+                           news_items=news_items.values())
